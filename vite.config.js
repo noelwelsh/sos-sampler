93decb0d9ed0eff813ac7843cbe29a1302372ebf
Noel Welsh <noel@noelwelsh.com>
2024-08-27 11:30:23 +0100

    Use relative paths in HTML output
    Since Github deploys pages under a subdirectory (e.g. `/sos-sampler`) we
    need to use relative, not absolute, references to artifacts

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
+  base: "./", // Use ./ to ensure relative paths
   plugins: [scalaJSPlugin()],
 });
